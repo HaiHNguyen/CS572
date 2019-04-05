23dcc026cb072500827c733d0a8aecc94122a6a2
HaiHNguyen <haihong.nguyen.vn@gmail.com>
2019-04-04 23:16:50 -0500

    Practicing 4 - Web Server, handling POST and GET request

---
+++ practices/ReactiveWebServer.js
@@ -0,0 +1,89 @@
+
+const {Subject} = require('rxjs');
+const {Url} = require('url');
+const { fork } = require('child_process');
+const { parse } = require('querystring');
+
+const subject = new Subject();
+function handleRequest(reqResp){
+
+    console.log("Method:", reqResp.req.method);
+    if(reqResp.req.method === 'GET'){
+        processGetRequest(reqResp);
+    }else if(reqResp.req.method === 'POST' ){
+        processPostRequest(reqResp);
+    }else{
+        reqResp.resp.end(reqResp.req.method + 'is not supported');
+    }
+}
+subject.subscribe(handleRequest);
+
+const http = require('http');
+http.createServer((req, resp) =>{
+        subject.next({'req': req, 'resp': resp})
+    }).listen(8888, ()=>console.log('Server started and listening on port 8888!'));
+
+
+function processGetRequest(reqResp){
+    const url = new Url();
+    const queryString =  JSON.parse(JSON.stringify(url.parse(reqResp.req.url, true).query));
+    if(Object.keys(queryString).length !== 0) {
+
+        reqResp.resp.writeHead('200', {'Content-Type': 'text/html'});
+        reqResp.resp.write("<p>Request processing...</p>");
+
+        const fileName = queryString['file'];
+        if(fileName != undefined) {
+            const readingFile = fork('ReadFile.js');
+            //send fine name to child process
+            readingFile.send(fileName);
+
+            //Listen to message from child process
+            readingFile.on('message', data => {
+                //File does not exist
+                if(data == 'error'){
+                    reqResp.resp.write('<p>File does not exist!.</p>');
+                    reqResp.resp.end();
+                }else {
+                    console.log("data received!");
+                    if (data == 'done') {
+                        console.log("I am done receiving the file");
+                        reqResp.resp.write("<p>Data received successfully.</p>");
+                        reqResp.resp.end();
+                    } else {
+                        reqResp.resp.write(data);
+                    }
+                }
+            })
+
+            //listen to exit event of child process.
+            readingFile.on('exit', (code)=>{
+                console.log("Reading file process exited with code: ", code);
+            })
+
+        }else{
+            reqResp.resp.write("<p>Invalid URL.</p>");
+            reqResp.resp.end();
+        }
+    }
+}
+function processPostRequest(reqResp){
+
+    getRequestBody(reqResp.req, (requestBody) =>{
+            console.log(requestBody);
+            reqResp.resp.end();
+        });
+}
+
+function getRequestBody( req, callback){
+
+    let requestBody = '';
+    req.on('data', (chunk) =>{
+        requestBody += chunk.toString();
+    });
+
+    req.on('end', ()=>{
+        callback(parse(requestBody));
+    });
+
+}
