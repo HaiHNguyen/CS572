667817829847701d42ab5acbef9bb31e412a4c06
HaiHNguyen <haihong.nguyen.vn@gmail.com>
2019-04-04 21:38:21 -0500

    Exercise 4 - checking system using Subject

--- exercises/4/CheckSystemUsingSubject.js
+++ exercises/4/CheckSystemUsingSubject.js
@@ -10,30 +10,31 @@ const gbMem = Math.round(freeMem/ 1024/ 1024 / 1024 * 100) / 100;
 
 const subject = new Subject();
 //event1
-subject.subscribe((dCtC) => {
-    if(dCtC[0] < 4)
+subject.subscribe((data) => {
+    if(data[0] < 4)
     {
-        console.log('Memory in GB you have ', dCtC[0]);
+        console.log('Memory in GB you have ', data[0]);
         console.log('The app needs at lest 4 GB of RAM!');
     }
 });
 //event2
-subject.subscribe((dCtC) => {
-    if( dCtC[1] < 2)
+subject.subscribe((data) => {
+    if( data[1] < 2)
     {
-        console.log('Processors you have ', dCtC[1]);
+        console.log('Processors you have ', data[1]);
         console.log('Processor is not supported!');
     }
 
+
 });
 //event3
-subject.subscribe((dCtC) => {
-    if(dCtC[0] >= 4 && dCtC[1] >= 2)
+subject.subscribe((data) => {
+    if(data[0] >= 4 && data[1] >= 2)
     {
         console.log('System is checked successfully :-)!');
-        console.log('\tYou have ', dCtC[0], 'GB of RAM');
-        console.log('\tYou have ', dCtC[1], 'CPU cores');
+        console.log('\tYou have ', data[0], 'GB of RAM');
+        console.log('\tYou have ', data[1], 'CPU cores');
     }
 });
-const dCtC = [gbMem, noCPUs];
-subject.next(dCtC);
+const data = [gbMem, noCPUs];
+subject.next(data);
